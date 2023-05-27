3235050412d630b1d8a634b93b10606d58f2033e
Ruwan <ruwanliyanagegalle1994@gmail.com>
2023-05-27 17:48:57 +0530

    init commit

---
+++ src/main/java/lk/premier/league/lpl/LplApplication.java
@@ -0,0 +1,13 @@
+package lk.premier.league.lpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LplApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/premier/league/lpl/LplApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.premier.league.lpl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
